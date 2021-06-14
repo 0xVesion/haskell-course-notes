dcab52ec50ca8cca64b804f5a3760a60f7b01738
Vincent Engel <ve@vesion.io>
2021-06-14 20:47:53 +0000

    Continued working on lessong 15 high order functions

--- lesson-15-high-order-functions.hs
+++ lesson-15-high-order-functions.hs
@@ -36,4 +36,73 @@ applyTwice :: (a -> a) -> a -> a
 applyTwice fun x = fun (fun x)
 
 myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith _ [] _ = []
+myZipWith _ _ [] = []
 myZipWith f (a : as) (b : bs) = f a b : myZipWith f as bs
+
+myFlip :: (a -> b -> c) -> (b -> a -> c)
+myFlip f a b = f b a
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap _ [] = []
+myMap fun (el : rest) = fun el : myMap fun rest
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter _ [] = []
+myFilter predicate (x : xs)
+  | predicate x = x : myFilter predicate xs
+  | otherwise = myFilter predicate xs
+
+myShorterQuickSort :: (Ord a) => [a] -> [a]
+myShorterQuickSort [] = []
+myShorterQuickSort (x : xs) = smallerNumbers ++ [x] ++ largerNumbers
+  where
+    smallerNumbers = myShorterQuickSort (filter (< x) xs)
+    largerNumbers = myShorterQuickSort (filter (> x) xs)
+
+largNum :: (Integral a) => a
+largNum = head (filter isDivisible [100000, 99999 ..])
+  where
+    isDivisible x = (x `mod` 3829) == 0
+
+oddSquares :: (Integral a) => a
+oddSquares =
+  sum
+    ( takeWhile
+        (< 10000)
+        ( filter
+            odd
+            ( map (^ 2) [1 ..]
+            )
+        )
+    )
+
+oddSquaresLc :: (Integral a) => a
+oddSquaresLc = sum (takeWhile (< 10000) ([x | a <- [1 ..], let x = a ^ 2, odd x]))
+
+collatzSequence :: (Integral a) => a -> [a]
+collatzSequence 1 = [1]
+collatzSequence x
+  | even x = x : collatzSequence (x `div` 2)
+  | otherwise = x : collatzSequence (x * 3 + 1)
+
+collatzSequenceTest :: Int
+collatzSequenceTest =
+  length
+    ( filter
+        (> 15)
+        ( map
+            (length . collatzSequence)
+            [1 .. 100]
+        )
+    )
+
+mySum :: (Foldable t, Num b) => t b -> b
+mySum = foldl (+) 0
+
+
+myElem :: (Foldable t, Eq a) => a -> t a -> Bool
+myElem y = foldl (\acc x -> if x == y then True else acc) False
+
+myMap2 :: Foldable t1 => (t2 -> a) -> t1 t2 -> [a]
+myMap2 fun = foldr (\x acc -> (fun x) : acc) []
