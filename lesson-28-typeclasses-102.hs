8d398d153b4b7301868c5c3d6b594fc6d584e427
Vincent Engel <ve@vesion.io>
2021-07-04 12:09:40 +0000

    Finished lesson 28 type classes 102

---
+++ lesson-28-typeclasses-102.hs
@@ -0,0 +1,14 @@
+module X where
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+  Red == Red = True
+  Green == Green = True
+  Yellow == Yellow = True
+  _ == _ = False
+
+instance Show TrafficLight where
+  show Red = "Red light"
+  show Yellow = "Yellow light"
+  show Green = "Green light"
